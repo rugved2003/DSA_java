1ae3c32403faf40c1bce14525027d5905fc9bb6e
Rugved Nakka <81752288+rugved2003@users.noreply.github.com>
2023-06-13 09:28:25 +0530

    Add files via upload

---
+++ Linearsearch.java
@@ -0,0 +1,19 @@
+public class Linearsearch {
+    public static void Main(int[] arr, int item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                System.out.println(item + " Found at index : " + i);
+                return;
+            }
+        }
+        System.out.println("Not found");
+
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { 12, 5, 18, 25, -3, 19 };
+
+        int item = 25;
+        Main(arr, item);
+    }
+}
